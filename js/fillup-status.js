df941db8405d2fbe66b670a7617ac0dc0ba85d9f
dhij <davidhwang.ij@gmail.com>
2024-04-28 00:44:54 +0000

    Updated site

--- js/fillup-status.js
+++ js/fillup-status.js
@@ -1,15 +1,15 @@
-//generate 1, 2 or 3
-function fillupRoll() {
-    return Math.floor(Math.random() * 3) + 1; // Generates a random number of moves vanillaly
+//generate number up to parameter
+function fillupRoll(x) {
+    return Math.floor(Math.random() * x) + 1; // Generates a random number of moves vanillaly
 }
 
 // update paragraph with the rolled number from fillupRoll()
 function displayRoll(){
-    var rollNumber=fillupRoll();
+    var rollNumber=fillupRoll(3);
     document.getElementById('rollNumber').innerText = 'Number of moves: ' + rollNumber;
 }
 
-//event listen to button
+//event listen to roll button
 document.getElementById('rollButton').addEventListener('click', displayRoll);
 
 //event to display number from slider
@@ -19,4 +19,14 @@ output.innerHTML = slider.value;
 
 slider.oninput = function() {
   output.innerHTML = this.value;
+}
+
+//event listen to coordinate button
+document.getElementById('rollCoordinatesButton').addEventListener('click', displayCoordinateRoll);
+
+// update paragraph with the rolled number from the coordinates()
+function displayCoordinateRoll(){
+  var rollNumberX=fillupRoll(11);
+  var rollNumberY=fillupRoll(11);
+  document.getElementById('rollCoordinates').innerText = 'Chosen Coordinates: ' + rollNumberX + ', ' + rollNumberY;
 }
